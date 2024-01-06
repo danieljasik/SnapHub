4d7c24c89a1bc17078fe5c084213de3db8c6aa40
Daniel Jasik <daniel11jasik@gmail.com>
2024-01-06 16:48:06 +0100

    portfolioFix

--- SnapHub/Data/Migrations/20240106154708_portfolioNew.cs
+++ SnapHub/Data/Migrations/20240106154708_portfolioNew.cs
@@ -4,11 +4,15 @@
 
 namespace SnapHub.Data.Migrations
 {
-    public partial class PortfolioTest2 : Migration
+    public partial class portfolioNew : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            
+            migrationBuilder.InsertData(
+            table: "Portfolio",
+            columns: new[] { "Id", "Title" },
+            values: new object[] { 1, "Portfolio" }
+            );
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
